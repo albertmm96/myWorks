5995b55f66be3735c7700892dd9a5416a838ce97
albertmm96 <a_m_monfared@outlook.com>
2025-07-25 03:01:48 +0200

    Qt integration includes recognized

---
+++ AsebanSkyWatch/AsebanSkyWatch.cpp
@@ -0,0 +1,13 @@
+﻿// AsebanSkyWatch.cpp : définit le point d'entrée de l'application.
+//
+
+#include "AsebanSkyWatch.h"
+#include <QApplication>
+#include <QPushButton>
+
+int main(int argc, char* argv[]) {
+    QApplication app(argc, argv);
+    QPushButton button("Hello from Qt!");
+    button.show();
+    return app.exec();
+}
