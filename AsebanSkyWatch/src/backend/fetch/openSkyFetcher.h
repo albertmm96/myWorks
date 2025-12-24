36f9d30d0acc5a18ae5a3969c603cb3ef9fe451a
Albert Mansouri Monfared <a.mansourimonfared@gmail.com>
2025-12-24 20:04:16 +0100

    Flights legacy table & all dependencies cleanup
    The  table was meant to be used later for historical data, but even then, involving an external Python script would only add extra runtime latency. Also, updating the C++/Qt/Python pipelines has an extra cost we can easily avoid for now.
    
    Historical data can later be stored in separate table, triggered by the analysis parts in the UI, on demand and only for specific flights (in terms of computational and storage constraint, it doesn't make sense to pull historical data for an entire fetched file with tens and hundreds of real time flights)

--- AsebanSkyWatch/src/backend/fetch/openSkyFetcher.h
+++ AsebanSkyWatch/src/backend/fetch/openSkyFetcher.h
@@ -14,8 +14,6 @@ class OpenSkyFetcher : public QObject {
 
 public:
     explicit OpenSkyFetcher(QObject* parent = nullptr);
-    bool runPythonFlightFetcher(const QString& icao24, qint64 begin, qint64 end);
-    void parseAndInsertFlights(const QString& filePath, QSqlDatabase& db);
     void setCredentialsPath(const QString& path) { credentialsPath_ = path; }
     void fetchStatesBBox(double minLat, double minLon,
         double maxLat, double maxLon,
--- AsebanSkyWatch/src/main/main.cpp
+++ AsebanSkyWatch/src/main/main.cpp
@@ -72,28 +72,6 @@ int main(int argc, char* argv[])
     // we drop existing tables if any
     runQuery(query, "DROP TABLE IF EXISTS states_live CASCADE;");
     runQuery(query, "DROP TABLE IF EXISTS weather_live CASCADE;");
-    runQuery(query, "DROP TABLE IF EXISTS flights CASCADE;");
-
-	// we keep this for now, may become legacy later or just removed
-    runQuery(query,
-        "CREATE TABLE IF NOT EXISTS flights ("
-        "icao24 TEXT, "
-        "callsign TEXT, "
-        "estDepartureAirport TEXT, "
-        "estArrivalAirport TEXT, "
-        "firstSeen INTEGER, "
-        "lastSeen INTEGER, "
-        "estDepartureAirportHorizDistance INTEGER, "
-        "estArrivalAirportHorizDistance INTEGER, "
-        "estDepartureAirportVertDistance INTEGER, "
-        "estArrivalAirportVertDistance INTEGER, "
-        "departureAirportCandidatesCount INTEGER, "
-        "arrivalAirportCandidatesCount INTEGER, "
-        "time TIMESTAMPTZ DEFAULT NOW());"
-    );
-
-    // we turn it into a hypertable
-    runQuery(query, "SELECT create_hypertable('flights', 'time', if_not_exists => TRUE);");
 
     // we create OpenSky fetcher
     OpenSkyFetcher* fetcher = new OpenSkyFetcher;
@@ -105,26 +83,6 @@ int main(int argc, char* argv[])
     QObject::connect(fetcher, &OpenSkyFetcher::fetchError, [](const QString& error) {
         qWarning() << "OpenSky error:" << error;
         });
-
-    // we define a time window for historical data
-    QString icao24 = "e49c0d";
-    qint64 now = QDateTime::currentSecsSinceEpoch();
-    qint64 end = now;
-    qint64 begin = end - 21600; // 6 hours before that = 6 x 3600 s
-    // call Python script and insert into DB if successful
-    if (fetcher->runPythonFlightFetcher(icao24, begin, end)) {
-        QDir dir(QCoreApplication::applicationDirPath());
-        // going up 3 levels
-        dir.cdUp();
-        dir.cdUp();
-        dir.cdUp();
-        // Now go into /src/backend/fetch
-        dir.cd("src/backend/fetch");
-        QString jsonPath = dir.absolutePath() + "/flights.json";
-
-        // insert using the flights connection (named connection)
-        fetcher->parseAndInsertFlights(jsonPath, dbFlights);
-    }
 
     // live states (OpenSky /api/states/all) stored while the app runs
     runQuery(query,
